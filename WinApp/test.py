02dbfec42802e53a3eed29fa750ce835088bf9c1
JannickRichter <jannickrichter@web.de>
2025-02-17 09:04:29 +0100

    feat: implement login functionality and retrieve grades for specific subject

--- WinApp/test.py
+++ WinApp/test.py
@@ -1,6 +1,18 @@
 from edupage_connect import EdupageAPI
+from edupage_api import Grades
+from edupage_api import Term
+from edupage_api import Subjects
 
 edupage = EdupageAPI()
-edupage.getAverage()
+edupage.login("jannickrichter@web.de", "JRNinjago2007!?", "duden-gymn")
 
-print(edupage.getAverage())
+print(edupage.isLoggedIn())
+subjects = Subjects(edupage.edupage)
+grades = []
+grades_instance = Grades(edupage.edupage)  # Erstelle eine Instanz von Grades
+for grade in grades_instance.get_grades(term=Term.FIRST, year=2023):
+    if grade.subject_name == "MA":
+        grades.append(grade)
+print(grades)
+#print(subjects.get_subjects())
+#print(grades_instance.get_grades(term=Term.SECOND, year=2023))
