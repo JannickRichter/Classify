d86ec4cc1a7a7a78efaa61690dcdb07fc80f88bf
JRNinj <JannickRichter@web.de>
2025-03-02 21:23:27 +0100

    feat: remove test.py as it is no longer needed for version control

--- WinApp/test.py
+++
@@ -1,9 +0,0 @@
-from edupage_connect import EdupageAPI
-from edupage_api import Grades
-from edupage_api import Term
-from edupage_api import Subjects
-
-edupage = EdupageAPI()
-
-print(edupage.isLoggedIn())
-print(edupage.getAverage(2024, Term.FIRST))
